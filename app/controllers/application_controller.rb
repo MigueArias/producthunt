4886035c0d99b2eb41cbba84f3489e14a0a4e565
Migue Arias <miguearias@gmail.com>
2019-05-24 19:35:28 -0500

    Restringe la paginas privadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,15 +12,24 @@ class ApplicationController < ActionController::Base
     @current_user = nil
   end
 
+  
   private
     def signed_in?
       !current_user.nil?
     end
     helper_method :signed_in?
-
+    
     def current_user
       @current_user ||= User.find(cookies.signed[:user_id]) if cookies.signed[:user_id]
-      rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound
     end
     helper_method :current_user
+    
+    def private_access
+      redirect_to :login unless signed_in?
+    end
+
+    def public_access
+      redirect_to root_path if signed_in?
+    end
 end
